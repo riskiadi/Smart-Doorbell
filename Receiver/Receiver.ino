6b8ec4387acdc89b56f1faf3e2ad052bb7a4d7a0
Rizki Adi Saputra <ucupucupucup10@gmail.com>
2020-10-28 05:11:37 +0700

    create receiver device

---
+++ Receiver/Receiver.ino
@@ -0,0 +1,25 @@
+#include <ESP8266WiFi.h>
+#include <FirebaseArduino.h>
+#include <ESP8266HTTPClient.h>
+#include "Neotimer.h"
+#include "PinDefine.h"
+
+#include <ESP8266WiFiMulti.h>
+#include <WiFiClientSecure.h>
+
+//Define
+#define FIREBASE_HOST "manyaran-sistem.firebaseio.com"
+#define FIREBASE_AUTH "vGw7kpq6yTrIjPLVVciDBpqMoZxAkmaERSVRqZWt"
+#define WIFI_SSID "[Saputra]_plus"
+#define WIFI_PASSWORD "qwerty33"
+#define PIN_BUZZER D6
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
--- Smart-Doorbell.ino
+++ Smart-Doorbell.ino
@@ -1,6 +1,7 @@
 #include <ESP8266WiFi.h>
 #include <FirebaseArduino.h>
 #include <ESP8266HTTPClient.h>
+#include "Neotimer.h"
 #include "PinDefine.h"
 
 #include <ESP8266WiFiMulti.h>
@@ -10,8 +11,8 @@
 //Define
 #define FIREBASE_HOST "manyaran-sistem.firebaseio.com"
 #define FIREBASE_AUTH "vGw7kpq6yTrIjPLVVciDBpqMoZxAkmaERSVRqZWt"
-#define WIFI_SSID "[Saputra-Mobile]"
-#define WIFI_PASSWORD "alkalynx"
+#define WIFI_SSID "[Saputra]_plus"
+#define WIFI_PASSWORD "qwerty33"
 #define PIN_BUZZER D6
 #define PIN_TOUCH D8
 
@@ -19,48 +20,34 @@ int httpCode = 0;
 int fcmHttpCode = 0;
 BearSSL::WiFiClientSecure client;
 HTTPClient http;
-
-//SHA1 finger print of certificate use Firefox to view and copy
-const char* fingerprint = "06 0C 93 F2 CA 99 33 1C 00 A2 2D FF 4C C1 1C 89 4E C2 2E 75";
-
-int testCounter = 1;
+Neotimer notificationDelay;
 
 void setup() {
   Serial.begin(9600);
   delay(10);
+  notificationDelay.set(4000);
 
   //Initiate Wifi
   wifiInitialization();
 
   //Initiate Firebase
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-
-  //  sendDataToFirebase();
   
 }
 
 void loop() {
 
-//  Serial.println(digitalRead(PIN_TOUCH));
-//  if(digitalRead(PIN_TOUCH) == 1){
-//    Firebase.setBool("doorbell/isOn", true);
-//  }else{
-//    Firebase.setBool("doorbell/isOn", false);
-//  }
-//  delay(1);
-
-  if(testCounter%2==0){
-    Firebase.setBool("doorbell/isOn", false);
-    sendNotification();
-  }else{
+  Serial.println(digitalRead(PIN_TOUCH));
+  if(digitalRead(PIN_TOUCH) == 1){
     Firebase.setBool("doorbell/isOn", true);
-    sendNotification();
+    if(notificationDelay.repeat()){
+      sendNotification();
+    }
+  }else{
+    Firebase.setBool("doorbell/isOn", false);
   }
-  testCounter++;
-  delay(5000);
-  //test aja hapus aja
-
-
+  delay(10);
+  
 }
 
 
@@ -70,18 +57,17 @@ void sendNotification(){
 
  client.setInsecure();
  client.setTimeout(15000);
- int retries = 6;
- while(!client.connect("fcm.googleapis.com", 443) && (retries-- > 0)) {
-   Serial.print(".");
-   delay(1000);
- }
- Serial.println();
+ client.connect("fcm.googleapis.com", 443); 
+ //Uncoment for can retry if failed to connect
+// int retries = 6;
+// while(!client.connect("fcm.googleapis.com", 443) && (retries-- > 0)) {
+//   Serial.print(".");
+//   delay(1000);
+// }
  if(!client.connected()) {
-    Serial.println("Failed to connect using insecure client, try to use fingerprint.. going back to sleep.");
-    client.stop();
-    return;
+    Serial.println("Failed to connect using insecure client, check internet connection or try to use fingerprint..");
  }else{
-    Serial.println("Success to connect.");
+    Serial.println("[Success to connect]");
     Serial.println("--------");
     Serial.print("Sending post request to: ");
     Serial.println("https://fcm.googleapis.com/fcm/send");
@@ -89,8 +75,8 @@ void sendNotification(){
     http.addHeader("User-Agent", "PostmanRuntime/7.26.5");
     http.addHeader("Connection", "keep-alive");
     http.addHeader("Content-Type", "application/json");
-    http.addHeader("Authorization", "key=AAAAWKZPac8:APA91bEZuPi9ZsYT7uQtXEQVpdTMSAfsEZVkZiXJ4i_yM6mxBeGD12i6oM-hWZvN01MsiInMpyFUiVbJCUJWPAKjagyev4sdw6mBAGe0LU0Yr4y2jAZ6ulc_xaXkaveBx5MEdqQdDPUK");
-    fcmHttpCode = http.POST("{\"to\": \"/topics/beritaku\", \"topic\": \"beritaku\", \"notification\": { \"title\": \"Breaking News\", \"body\": \"New news available.\", \"sound\": \"default\"}}");
+    http.addHeader("Authorization", "key=AAAAAs30-qE:APA91bH2N8b_Lfp7B4aKMbKwPFedzwVWP3ffe_gPbwLdIE4jStahP5dQZ3AuVRnoGum-1LU8iWQZ8gT5DnkGYKL66LBN3w7nMYZYOwSiaa7IQZEEDWV64HTmnctWPzBvzne3gYmcWunn");
+    fcmHttpCode = http.POST("{ \"to\": \"/topics/Doorbell\", \"topic\": \"Doorbell\", \"notification\": { \"title\": \"MANYARAN SISTEM\", \"body\": \"Seseorang mengunjungi rumah anda.\", \"sound\": \"notification.mp3\", \"android_channel_id\" : \"manyaran_id\", \"click_action\" : \"FLUTTER_NOTIFICATION_CLICK\" } }");
     Serial.print("Post request completed, ");
     Serial.print("status code: ");
     Serial.println(fcmHttpCode);
@@ -99,7 +85,7 @@ void sendNotification(){
  }
 
   client.stop();
- 
+
 }
 
 
